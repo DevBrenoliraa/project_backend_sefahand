27fa2ab478a1327fcd7f4133bc0fa8204a588408
Breno Lira Rocha <brenolirarocha5@gmail.com>
2025-11-12 08:26:31 -0300

    autualizacao da rota de funcionarios

--- backend/src/features/routes/funcionarioRoutes.js
+++ backend/src/features/routes/funcionarioRoutes.js
@@ -4,7 +4,7 @@ import { listarFuncionarios, criarfuncionario } from '../controllers/funcionario
 
 const routes = Router();
 
-routes.get('/epis', verificarToken, listarFuncionarios)
-routes.post('/epis/cadastro', verificarToken, criarfuncionario);
+routes.get('/funcionarios', verificarToken, listarFuncionarios)
+routes.post('/funcionario/cadastros', verificarToken, criarfuncionario);
 
 export default routes;
